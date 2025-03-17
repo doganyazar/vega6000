bd827a247e9cb6a915fdf0d446d4e2d1b8665ba3
Dogan Yazar <doganyazar@gmail.com>
2025-03-17 14:15:20 +0300

    Support running with node.js

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 This is a CLI tool to interact with the Vega 6000 CGI API and run test scenarios.
 
 ## Setup
-Bun is required to run this project. To install Bun, check out the [Bun website](https://bun.sh/docs/installation#installing), or run the following command:
+Bun is recommended to run this project. To install Bun, check out the [Bun website](https://bun.sh/docs/installation#installing), or run the following command:
 
 ```bash
 npm install -g bun
@@ -28,4 +28,13 @@ Can set environment variables in the command line as well:
 
 ```bash
 HOST=10.101.200.201 bun run cli.ts
+```
+
+## Run with Node.js
+It is possible to run it with Node >= v22 as well if installing bun is not preferred:
+
+```bash
+npm install
+# set desired host below
+HOST=10.101.200.201 npm run dev-node
 ```
--- scenarios.ts
+++ scenarios.ts
@@ -1,94 +1,105 @@
-import { type ImageSize, type SDIPort, type StreamConfigInput, type VideoCodec } from './StreamApi'
+import {
+  type ImageSize,
+  type SDIPort,
+  type StreamConfigInput,
+  type VideoCodec,
+} from "./StreamApi";
 
 interface MakeStreamOpts {
-    id: SDIPort
-    video: {
-        codec: VideoCodec
-        bitrate?: number
-        imageSize?: ImageSize
-    }
-    audio: {
-        codec: 'aac_lc' | 'ac3'
-        bitrate?: number
-    }[]
-    rtpPort: number
+  id: SDIPort;
+  video: {
+    codec: VideoCodec;
+    bitrate?: number;
+    imageSize?: ImageSize;
+  };
+  audio: {
+    codec: "aac_lc" | "ac3";
+    bitrate?: number;
+  }[];
+  rtpPort: number;
 }
 
 function makeStream({
-    id,
-    video: { codec, bitrate = 15000, imageSize = '1280,720' },
-    audio,
-    rtpPort,
+  id,
+  video: { codec, bitrate = 15000, imageSize = "1280,720" },
+  audio,
+  rtpPort,
 }: MakeStreamOpts): StreamConfigInput {
-    const audioConfig = audio.map(({ codec, bitrate = 64000 }) => ({
+  const audioConfig = audio.map(({ codec, bitrate = 64000 }) => ({
+    codec,
+    bitrate,
+    sampleRate: 48000,
+    pair: 1 as const,
+  }));
+
+  return {
+    id,
+    encoding: {
+      video: {
         codec,
         bitrate,
-        sampleRate: 48000,
-        pair: 1 as const,
-    }))
-
-    return {
-        id,
-        encoding: {
-            video: {
-                codec,
-                bitrate,
-                imageSize,
-                pixelFormat: 'XV20',
-            },
-            audio: audioConfig,
-        },
-        output: {
-            url: `rtp://127.0.0.1:${rtpPort}`,
-        },
-    }
+        imageSize,
+        pixelFormat: "XV20",
+      },
+      audio: audioConfig,
+    },
+    output: {
+      url: `rtp://127.0.0.1:${rtpPort}`,
+    },
+  };
 }
 
 function makeSimilarStreams(opts: MakeStreamOpts, count: number) {
-    return Array.from({ length: count }, (_, i) => {
-        return makeStream({
-            ...opts,
-            id: (i + 1) as SDIPort,
-            rtpPort: opts.rtpPort + i * 10,
-        })
-    })
+  return Array.from({ length: count }, (_, i) => {
+    return makeStream({
+      ...opts,
+      id: (i + 1) as SDIPort,
+      rtpPort: opts.rtpPort + i * 10,
+    });
+  });
 }
 
-export function Encode_Main(count: number, videoCodec: VideoCodec): StreamConfigInput[] {
-    const streams = makeSimilarStreams(
-        {
-            id: 1,
-            video: {
-                codec: videoCodec,
-                bitrate: 15000,
-                imageSize: '1280,720',
-            },
-            audio: [{ codec: 'aac_lc' }],
-            rtpPort: 4010,
-        },
-        count
-    )
-    console.log('Streams', streams)
-    return streams
+export function Encode_Main(
+  count: number,
+  videoCodec: VideoCodec
+): StreamConfigInput[] {
+  const streams = makeSimilarStreams(
+    {
+      id: 1,
+      video: {
+        codec: videoCodec,
+        bitrate: 15000,
+        imageSize: "1280,720",
+      },
+      audio: [{ codec: "aac_lc" }],
+      rtpPort: 4010,
+    },
+    count
+  );
+  console.log("Streams", JSON.stringify(streams, null, 2));
+  return streams;
 }
 
-export const Encode_Double_Stereo = (count: number, videoCodec: VideoCodec): StreamConfigInput[] => {
-    const streams = makeSimilarStreams(
-        {
-            id: 1,
-            video: {
-                codec: videoCodec,
-                bitrate: 15000,
-                imageSize: '1280,720',
-            },
-            audio: [
-                { codec: 'aac_lc', bitrate: 64000 },
-                { codec: 'ac3', bitrate: 48000 },
-            ],
-            rtpPort: 4010,
-        },
-        count
-    )
-    console.log('Streams', streams)
-    return streams
-}
+export const Encode_Double_Stereo = (
+  count: number,
+  videoCodec: VideoCodec
+): StreamConfigInput[] => {
+  const streams = makeSimilarStreams(
+    {
+      id: 1,
+      video: {
+        codec: videoCodec,
+        bitrate: 15000,
+        imageSize: "1280,720",
+      },
+      audio: [
+        { codec: "aac_lc", bitrate: 64000 },
+        { codec: "ac3", bitrate: 48000 },
+      ],
+      rtpPort: 4010,
+    },
+    count
+  );
+  console.log("Streams", JSON.stringify(streams, null, 2));
+  return streams;
+};
